bd193d0119c7a569ce0399a55b595880d991c48c
IceCandy <54431129@qq.com>
2025-06-22 16:44:19 +0800

    New branch condition and bashrc

--- link-dotFiles.sh
+++ link-dotFiles.sh
@@ -1,9 +1,17 @@
-#!/bin/zsh
-
 DOTFILES_DIR="$HOME/dotFiles"
 
-ln -sf "$DOTFILES_DIR/zsh/.zshrc" ~/.zshrc
-ln -sf "$DOTFILES_DIR/vim/.vimrc" ~/.vimrc
-ln -sf "$DOTFILES_DIR/tmux/.tmux.conf" ~/.tmux.conf
+if [[ "$SHELL" == "/bin/zsh" ]] then
+	echo "zshrc"
+	ln -sf "$DOTFILES_DIR/zsh/.zshrc" ~/.zshrc
+elif [[ "$SHELL" == "/bin/bash" ]] then
+	echo "bashrc"
+	ln -sf "$DOTFILES_DIR/bash/.bashrc" ~/.bashrc
+else 
+	echo "error"
+fi
+	echo "vimrc"
+	ln -sf "$DOTFILES_DIR/vim/.vimrc" ~/.vimrc
+	echo "tmuxrc"
+	ln -sf "$DOTFILES_DIR/tmux/.tmux.conf" ~/.tmux.conf
 
 
